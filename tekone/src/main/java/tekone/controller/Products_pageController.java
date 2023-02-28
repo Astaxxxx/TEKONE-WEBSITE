5a21e9f8cfa1d93d6ace9311e9962f29e021fd53
Usmaan Malik <200104199@aston.ac.uk>
2023-02-28 14:05:42 +0000

    edit

---
+++ tekone/src/main/java/tekone/controller/Products_pageController.java
@@ -0,0 +1,21 @@
+package tekone.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Products_pageController {
+   @RequestMapping(value = "/products_page")
+   public String products_page() {
+      return "products_page";
+   }
+}
